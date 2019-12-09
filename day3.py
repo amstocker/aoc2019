5f6b3460fb80343f8850c358e7f07ed1a9fdf8b5
Andrew Stocker <sleepyams@gmail.com>
2019-12-08 19:43:22 -0700

    day3

--- day3.py
+++ day3.py
@@ -1,9 +1,9 @@
 with open("day3.txt") as f:
     data = f.read().split('\n')
-    wire1 = data[0].rstrip().split(',')
-    wire2 = data[1].rstrip().split(',')
+    data1 = data[0].rstrip().split(',')
+    data2 = data[1].rstrip().split(',')
 
-def travel_once(pos, inst, path):
+def travel_once(pos, inst, path, steps, step):
     direction = inst[0]
     dist = int(inst[1:])
     while dist > 0:
@@ -15,23 +15,32 @@ def travel_once(pos, inst, path):
             pos = (pos[0] - 1, pos[1])
         if direction == "D":
             pos = (pos[0], pos[1] - 1)
-        path.add(pos)
         dist -= 1
-    return pos
+        step += 1
+        path.add(pos)
+        steps[pos] = step
+    return pos, step
 
-def travel(inst_list):
+def travel(instruction_list):
     pos = (0,0)
+    step = 0
     path = set()
-    for inst in inst_list:
-        pos = travel_once(pos, inst, path)
-    return path
+    steps = {}
+    for inst in instruction_list:
+        pos, step = travel_once(pos, inst, path, steps, step)
+    return path, steps
 
 def norm(p):
     return abs(p[0]) + abs(p[1])
 
 
+# do travelling
+wire1_path, wire1_steps = travel(data1)
+wire2_path, wire2_steps = travel(data2)
+
 # part 1
-wire1_path = travel(wire1)
-wire2_path = travel(wire2)
 common = wire1_path.intersection(wire2_path)
 print(min(map(norm, common)))
+
+# part 2
+print(min(map(lambda p: wire1_steps[p] + wire2_steps[p], common)))
